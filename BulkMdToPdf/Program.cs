78d4c5d7b5627c5ca7bd4ab0834b9e95d28ebce2
Ovis <shohei.yasue@sirocco.team>
2019-02-28 00:39:18 +0900

    引数指定がない場合のエラーメッセージをログに出力

--- BulkMdToPdf/Program.cs
+++ BulkMdToPdf/Program.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Linq;
 using BulkMdToPdf.Domain;
+using NLog;
 
 namespace BulkMdToPdf
 {
     class Program
     {
+        private static ILogger Logger = LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
             var setting = new Setting();
 
             if (args.FirstOrDefault() == null)
             {
+                Logger.Error("Error: DocumentPath is invalid.");
                 Console.WriteLine("Error: DocumentPath is invalid.");
             }
 
